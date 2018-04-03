5f4bb371f64e1e2e6b2df1357d9ac3000065dbb5
Vaibhav Narkhede <Vaibhav-Narkhede@users.noreply.github.com>
2018-04-03 10:10:53 +0530

    Create LazySample.kt

---
+++ LazySample.kt
@@ -0,0 +1,12 @@
+class LazySample {
+    val lazy: String by lazy {
+        println("computed!")
+        "my lazy"
+    }
+}
+
+fun main(args: Array<String>) {
+    val sample = LazySample()
+    println("lazy = ${sample.lazy}")
+    println("lazy = ${sample.lazy}")
+}
