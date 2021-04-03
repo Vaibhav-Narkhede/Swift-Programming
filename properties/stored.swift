310dc702cb57c5370214b37421eae92a3f69b3de
Vaibhav Narkhede <Vaibhav-Narkhede@users.noreply.github.com>
2021-04-03 23:44:50 +0530

    Create stored.swift

---
+++ properties/stored.swift
@@ -0,0 +1,11 @@
+print("-----Stored Property-----")
+
+struct Employee{
+    var name: String
+}
+
+//let e1 = Employee(name: "Garry")
+var e1 = Employee(name: "Garry")
+print(e1)
+e1.name = "Aryan"
+print(e1)
