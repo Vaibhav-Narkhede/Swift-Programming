2bb3dccc08fd117d572f560126ed64b299adf580
Vaibhav Narkhede <Vaibhav.Narkhede@outlook.com>
2021-09-11 21:50:52 +0530

    Added playground file

---
+++ iOS_applications/SwiftUI_GRDB/SwiftUI_GRDB/DBManager.swift
@@ -0,0 +1,8 @@
+//
+//  DBManager.swift
+//  SwiftUI_GRDB
+//
+//  Created by Orion on 10/09/21.
+//
+
+import Foundation
---
+++ iOS_applications/SwiftUI_GRDB/SwiftUI_GRDB/SwiftUI_GRDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GRDBApp.swift
+//  SwiftUI_GRDB
+//
+//  Created by Orion on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GRDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
